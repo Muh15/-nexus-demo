0e00948ce7e768be0be211c19039a1ecafa53450
Muh15 <149613890+Muh15@users.noreply.github.com>
2026-09-02 21:28:51 +0300

    Export business action connector

--- mvp/connectors/__init__.py
+++ mvp/connectors/__init__.py
@@ -1,6 +1,7 @@
-"""Source connectors for the NEXUS MVP."""
+"""Source and action connectors for the NEXUS MVP."""
 
 from .base import Connector, ConnectorResult
+from .business_api_action import BusinessActionConfig, BusinessActionConnector
 from .business_api_connector import BusinessApiConfig, BusinessApiConnector
 from .file_connector import FileConnector, IngestedRecord
 from .http_json_connector import HttpJsonConfig, HttpJsonConnector
@@ -8,6 +9,8 @@ from .http_json_connector import HttpJsonConfig, HttpJsonConnector
 __all__ = [
     "Connector",
     "ConnectorResult",
+    "BusinessActionConfig",
+    "BusinessActionConnector",
     "BusinessApiConfig",
     "BusinessApiConnector",
     "FileConnector",
